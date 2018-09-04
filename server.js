4ab88c0602cbdd6d879cb1915bf1af8710b1e4b8
Alex <0alexgutierrez@gmail.com>
2018-09-04 15:17:00 -0700

    added GET notes list endpoint

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const data = require('./db/notes');
 const app = express();
 
 console.log('Hello Noteful!');
-// add static server here
 
+// add static server here
 app
   .listen(8080, function() {
     console.info(`Server listening on ${this.address().port}`);
@@ -16,3 +16,7 @@ app
   .on('error', err => {
     console.error(err);
   });
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
