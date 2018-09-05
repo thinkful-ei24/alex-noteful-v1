a34b4371a47e835f1e6580426a39bc111a170caa
Alex <0alexgutierrez@gmail.com>
2018-09-05 16:36:22 -0700

    added error-handling

--- server.js
+++ server.js
@@ -13,17 +13,6 @@ app.use(express.static('public'));
 
 app.use(requestLogger);
 
-console.log('Hello Noteful!');
-
-// add static server here
-app
-  .listen(PORT, function() {
-    console.info(`Server listening on ${this.address().port}`);
-  })
-  .on('error', err => {
-    console.error(err);
-  });
-
 app.get('/api/notes', (req, res) => {
   let { searchTerm } = req.query;
   if (searchTerm) {
@@ -40,3 +29,25 @@ app.get('/api/notes/:id', (req, res) => {
 
   res.json(newObj);
 });
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app
+  .listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  })
+  .on('error', err => {
+    console.error(err);
+  });
