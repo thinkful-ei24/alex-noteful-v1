589d690b339b3e7df678b87fb21c45398a137b24
Alex <0alexgutierrez@gmail.com>
2018-09-05 15:43:39 -0700

    added logger

---
+++ middleware/logger.js
@@ -0,0 +1,11 @@
+function requestLogger(req, res, next) {
+  const now = new Date();
+  console.log(
+    `${now.toLocaleDateString()} ${now.toLocaleTimeString()} ${req.method} ${
+      req.url
+    }`
+  );
+  next();
+}
+
+app.use(requestLogger);
